15b78713feceafe6d64bd637c3984206f41e07c0
Halit Burak Yeşildal <48446413+phyex0@users.noreply.github.com>
2022-01-16 13:57:42 +0300

    Add files via upload

---
+++ SRCTest/src/main/java/ModelViewControllerConsole.java
@@ -0,0 +1,66 @@
+import javax.xml.crypto.Data;
+import java.util.*;
+
+
+public class ModelViewControllerConsole {
+
+	public static void main(String[] args) throws Exception {
+		// Connect to database
+		connectToDatabase();
+
+		
+		// Model View Controller (MVC)
+		// Router knows all the controllers
+		Map<String, Controller> router = new HashMap<>();		
+		router.put("MainMenu", new Controller(new MainMenuView(), new NopModel()));
+		router.put("Department", new Controller(new DepartmentView(), new DepartmentModel()));
+
+		ViewData viewData = new ViewData("MainMenu", "");		
+		do {
+			// Model, View, and Controller
+			Controller controller = router.get(viewData.functionName);
+			ModelData modelData = controller.executeModel(viewData);
+			viewData = controller.getView(modelData, viewData.functionName, viewData.operationName);
+			
+			System.out.println();
+			System.out.println("-------------------------------------------------");
+			System.out.println();
+		}
+		while (viewData.functionName != null);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Program is ended...");
+
+
+		// Disconnect from database
+		disconnectFromDatabase();
+	}
+
+	
+	public static void connectToDatabase() {
+		DatabaseUtilities.host = "MONSTER-PC:1433";
+		DatabaseUtilities.databaseName = "AdventureWorks2019";
+		DatabaseUtilities.userName = "sa";
+		DatabaseUtilities.password = "ekmek";
+
+		try {
+			DatabaseUtilities.getConnection();
+		}
+		catch(Exception e) {
+			System.out.println("Exception occured : " + e);
+			return;
+		}		
+	}
+	
+	public static void disconnectFromDatabase() {
+		try {
+			DatabaseUtilities.disconnect();
+		}
+		catch(Exception e) {
+			System.out.println("Error disconnecting from database : " + e);
+			return;
+		}		
+	}
+	
+}
